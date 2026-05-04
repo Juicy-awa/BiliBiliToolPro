0fe810ff1f0a838e2dcbdde2a7788256a342cc06
Ray <zai7lou@outlook.com>
2026-05-04 19:00:24 +0800

    feat(11-01): create IApiApi — merge 7 api.bilibili.com interfaces with region organization

--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IRelationApi.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IRelationApi.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using Ray.BiliBiliTool.Agent.BiliBiliAgent.Dtos;
+﻿using Ray.BiliBiliTool.Agent.BiliBiliAgent.Dtos;
 using Ray.BiliBiliTool.Agent.BiliBiliAgent.Dtos.Relation;
 using Refit;
 
@@ -80,39 +79,4 @@ public interface IRelationApi
         [Header("Cookie")] string ck,
         [Header("Referer")] string referer = RelationApiConstant.ModifyReferer
     );
-}
-
-public enum FollowingsOrderType
-{
-    /// <summary>
-    /// 最常访问频率倒序
-    /// </summary>
-    [DefaultValue("attention")]
-    AttentionDesc,
-
-    /// <summary>
-    /// 关注时间倒序
-    /// </summary>
-    [DefaultValue("")]
-    TimeDesc,
-}
-
-public class RelationApiConstant
-{
-    /// <summary>
-    /// GetTags接口中的Referer
-    /// {0}为UserId
-    /// </summary>
-    public const string GetTagsReferer = "https://space.bilibili.com/{0}/fans/follow";
-
-    /// <summary>
-    /// CopyUpsToGroup接口中的Referer
-    /// {0}为UserId
-    /// </summary>
-    public const string CopyReferer = "https://space.bilibili.com/{0}/fans/follow?tagid=-1";
-
-    /// <summary>
-    /// ModifyRelation接口种的Referer
-    /// </summary>
-    public const string ModifyReferer = "https://space.bilibili.com/{0}/fans/follow?tagid={1}";
 }
